a12f15d71c359568b499e6eeb27d3e6422a0803a
Zi <104781216+ZairaBarry@users.noreply.github.com>
2023-01-09 13:32:29 -0400

    created a function to return a license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,41 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const fs = require ("fs")
+const fs = require("fs")
 
 
 function renderLicenseBadge(license) {
-  switch(license) {
-    case  "MIT" :
-      return  "\n![License](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  switch (license) {
+    case "MIT":
+      return "\n![License](https://img.shields.io/badge/License-MIT-yellow.svg)";
 
-      case "Apache License 2.0":
-        return "\n![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
-  
-      case "Mozila Public License 2.0":
-        return "\n![License](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
-  
-      default:
-        return "";
-  
-    }
-  };
-  
-  
+    case "Apache License 2.0":
+      return "\n![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+
+    case "Mozila Public License 2.0":
+      return "\n![License](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)";
+
+    default:
+      return "";
+
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
- 
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+
+    case "Apache License 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+
+    case "Mozila Public License 2.0":
+      return "https://opensource.org/licenses/MPL-2.0";
+
+    default:
+      return "";
+  }
+};
+
+
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
- 
+function renderLicenseSection(license) { }
+
 
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {}
-  return `# ${data.title}
+function generateMarkdown(data) { }
+return `# ${data.title}
 
   
 module.exports = generateMarkdown;
